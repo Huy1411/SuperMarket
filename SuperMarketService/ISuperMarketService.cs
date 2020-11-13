dc679c41a9aeacad571bbfb4ad2805d0a4df9f9f
Nguyen Thanh Tuan <tuantuna955@gmail.com>
2020-11-13 11:49:00 +0700

    first commit

---
+++ SuperMarketService/ISuperMarketService.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SuperMarketService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISuperMarketService" in both code and config file together.
+    [ServiceContract]
+    public interface ISuperMarketService
+    {
+        //Payment Services
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Customer/GetList"
+            )]
+        List<Payment> GetPayments();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Payment/Create"
+            )]
+        bool AddPayment(Payment payment);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Payment/Edit/{id}"
+            )]
+        bool EditPayment(string id, Payment payment);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Payment/Delete/{id}"
+            )]
+        bool DeletePayment(string id);
+
+        //Employee Services
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Employee/GetList"
+            )]
+        List<Employee> GetEmployees();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Employee/Create"
+            )]
+        bool AddEmployee(Employee employee);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Employee/Edit/{id}"
+            )]
+        bool EditEmployee(string id, Employee employee);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Employee/Delete/{id}"
+            )]
+        bool DeleteEmployee(string id);
+
+
+        //Role service
+
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Role/GetList"
+            )]
+        List<Role> GetRoles();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Role/Create"
+            )]
+        bool AddRole(Role role);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Role/Edit/{id}"
+            )]
+        bool EditRole(string id, Role Role);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Role/Delete/{id}"
+            )]
+        bool DeleteRole(string id);
+
+        //Provider service 
+        [OperationContract] // bắt đầu viết services phải định nghĩa contract 
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            RequestFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Provider/GetList"
+            )]
+        List<Provider> GetProviders();
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Provider/Create"
+            )]
+        bool AddProvider(Provider provider);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Provider/Edit/{id}"
+            )]
+        bool EditProvider(string id, Provider provider);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST",
+            RequestFormat = WebMessageFormat.Json,
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "api/v1/Provider/Delete/{id}"
+            )]
+        bool DeleteProvider(string id);
+    }
+}
---
+++ SuperMarketService/SuperMarketService.svc.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SuperMarketService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SuperMarketService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select SuperMarketService.svc or SuperMarketService.svc.cs at the Solution Explorer and start debugging.
+    public class SuperMarketService : ISuperMarketService
+    {
+        SuperMarketDBDataContext data = new SuperMarketDBDataContext();
+
+        public bool AddEmployee(Employee employee)
+        {
+            try
+            {
+                data.Employees.InsertOnSubmit(employee);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool AddPayment(Payment payment)
+        {
+            try
+            {
+                data.Payments.InsertOnSubmit(payment);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool AddProvider(Provider provider)
+        {
+            try
+            {
+                data.Providers.InsertOnSubmit(provider);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool AddRole(Role role)
+        {
+            try
+            {
+                data.Roles.InsertOnSubmit(role);
+                data.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteEmployee(string id)
+        {
+            try
+            {
+                var employee = data.Employees.Where(b => b.ID == int.Parse(id)).FirstOrDefault();
+                data.Employees.DeleteOnSubmit(employee);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeletePayment(string id)
+        {
+            try
+            {
+                var payment = data.Payments.Where(b => b.ID == int.Parse(id)).FirstOrDefault();
+                data.Payments.DeleteOnSubmit(payment);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+           
+        }
+
+        public bool DeleteProvider(string id)
+        {
+            try
+            {
+                var provider = data.Providers.Where(b => b.ID == int.Parse(id)).FirstOrDefault();
+                data.Providers.DeleteOnSubmit(provider);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool DeleteRole(string id)
+        {
+            try
+            {
+                var role = data.Roles.Where(b => b.ID == int.Parse(id)).FirstOrDefault();
+                data.Roles.DeleteOnSubmit(role);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool EditEmployee(string id, Employee newEmployee)
+        {
+            try
+            {
+                var employee = data.Employees.Where(b => b.ID == int.Parse(id)).FirstOrDefault();
+                data.Employees.DeleteOnSubmit(employee);
+                data.Employees.InsertOnSubmit(newEmployee);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool EditPayment(string id, Payment newPayment)
+        {
+            try
+            {
+                var payment = data.Payments.Where(b => b.ID== int.Parse(id)).FirstOrDefault();
+                data.Payments.DeleteOnSubmit(payment);
+                data.Payments.InsertOnSubmit(newPayment);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool EditProvider(string id, Provider newProvider)
+        {
+            try
+            {
+                var provider = data.Providers.Where(b => b.ID == int.Parse(id)).FirstOrDefault();
+                data.Providers.DeleteOnSubmit(provider);
+                data.Providers.InsertOnSubmit(newProvider);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public bool EditRole(string id, Role newRole)
+        {
+            try
+            {
+                var role = data.Roles.Where(b => b.ID == int.Parse(id)).FirstOrDefault();
+                data.Roles.DeleteOnSubmit(role);
+                data.Roles.InsertOnSubmit(newRole);
+                data.SubmitChanges();
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            try
+            {
+                var employees = (from employee in data.Employees select employee).ToList();
+                return employees;
+            }
+            catch { return null; }
+        }
+
+        public List<Payment> GetPayments()
+        {
+            try
+            {
+                var payments = (from payment in data.Payments select payment).ToList();
+                return payments;
+            }
+            catch { return null; }
+        }
+
+        public List<Provider> GetProviders()
+        {
+            try
+            {
+                var providers = (from provider in data.Providers select provider).ToList();
+                return providers;
+            }
+            catch { return null; }
+        }
+
+        public List<Role> GetRoles()
+        {
+            try
+            {
+                var roles = (from role in data.Roles select role).ToList();
+                return roles;
+            }
+            catch { return null; }
+        }
+    }
+}
